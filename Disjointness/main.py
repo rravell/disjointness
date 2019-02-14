7e787f39d89c440b7e6c9bed9e1ca676807ae59e
gsenno <gsenno@PC0867.ad.icfo.net>
2019-02-14 15:57:06 +0100

    Programa lineal para el dual de efficiency usando cdd.

--- Disjointness/main.py
+++ Disjointness/main.py
@@ -4,63 +4,78 @@ Created on Feb 12, 2019
 @author: rravell
 '''
 from bellpolytope import BellPolytope
-import picos as pic
+import cdd as cdd
 import numpy as np
 
 
 if __name__ == '__main__':
-    N=4
-    K=2
-    poly = BellPolytope(N,K+1)
-    vertices=np.array(poly.getVertices())
-    NumberOfCoefficients=(N*(K+1))**2
-    shape=np.shape(vertices)
-    NumberOfVertices=shape[0]
-    NumberOfAborts=N*((K+1)*N + K*N)
-    #NumberOfRows=NumberOfVertices + 2*NumberOfAborts
-    A=np.zeros((NumberOfVertices, NumberOfCoefficients))
-    #A[NumberOfVertices:NumberOfRows,0]=0
-    A[:,:]=vertices
-    coefficients=np.ones(NumberOfVertices)
-    #l=0
-    #s=0
-    '''for j in range (NumberOfVertices, NumberOfVertices + NumberOfCoefficients/(K+1) + 1):
-        A[j, (K+1)*l]=1
-        l=l+1
-      
-    for j in range (0, N*(K+1)):
-        A[NumberOfVertices + NumberOfCoefficients/(K+1) +K*j, NumberOfCoefficients/(N*(K+1))-2 + NumberOfCoefficients/(N*(K+1))*j]= 1
-        A[NumberOfVertices + NumberOfCoefficients/(K+1) + 1 + K*j, NumberOfCoefficients/(N*(K+1))-1 + NumberOfCoefficients/(N*(K+1))*j] = 1 
- 
-    for j in range (NumberOfVertices + NumberOfCoefficients/(K+1) + 1,NumberOfVertices + 2*NumberOfCoefficients/(K+1) + 1):
-        A[j,(K+1)*s]=-1
-        s=s+1
-
-    for j in range (0, N*(K+1)):
-        A[NumberOfVertices + 2*NumberOfCoefficients/(K+1) +K*j, NumberOfCoefficients/(N*(K+1))-2 + NumberOfCoefficients/(N*(K+1))*j]= -1
-        A[NumberOfVertices + 2*NumberOfCoefficients/(K+1) + 1 + K*j, NumberOfCoefficients/(N*(K+1))-1 + NumberOfCoefficients/(N*(K+1))*j] = -1 '''
+    inputs = 4
+    outputs = 2
+     
+    K = outputs+1
+    totalOutputs = K**2
+     
+    N=inputs
+    totalInputs = N**2
+     
+    nZeroConstraints = 2*totalInputs*(totalOutputs-(K-1)**2)
+    nLowerBoundOnDeterministicConstraints = (K**(2*N))
+    nUpperBoundOnDeterministicConstraints = (K**(2*N))
+    nConstraints =  nZeroConstraints + nLowerBoundOnDeterministicConstraints + nUpperBoundOnDeterministicConstraints
+    nVariables = (K*N)**2
+     
+    matrixHRepresentation=np.zeros((nConstraints,1+nVariables))
+     
+    #IMPOSING THAT THE BELL COEFFICIENTS FOR ABORT EVENTS ARE >= 0
+    rowNum = 0
+    for j in range (1, 1+K*(totalInputs)):
+        matrixHRepresentation[j-1, K*j]=1
+        rowNum+=1
+    base=rowNum
+    for j in range (0, totalInputs):
+        matrixHRepresentation[base + j, (K-1)*K+1 + totalOutputs*j]= 1
+        matrixHRepresentation[base + 1 + j, (K-1)*K+2 + totalOutputs*j] = 1
+        rowNum+=2 
+            
     
+    poly = BellPolytope(N,K)
+    #IMPOSING THAT THE BELL FUNCTIONAL IS LOWER BOUNDED BY N**2*(N**2-2) ON LOCAL DISTRIBUTIONS WITH ABORT
+    for vector in poly.getVertices():
+        matrixHRepresentation[rowNum,0]=N**2*(N**2-2)
+        matrixHRepresentation[rowNum,1:]=vector
+        rowNum+=1
     
+    #IMPOSING THAT THE BELL FUNCTIONAL IS UPPER BOUNDED BY 1 ON LOCAL DISTRIBUTIONS WITH ABORT    
+    for vector in poly.getVertices():
+        matrixHRepresentation[rowNum,0]=1
+        matrixHRepresentation[rowNum,1:]=-vector
+        rowNum+=1
+    
+    #IMPOSING THAT THE BELL COEFFICIENTS FOR ABORT EVENTS ARE <= 0
+    base=rowNum
+    for j in range (1,1+K*(totalInputs)):
+        matrixHRepresentation[base+j,K*j]=-1
+        rowNum+=1
+    base=rowNum
+    for j in range (0, totalInputs):
+        matrixHRepresentation[base + j, (K-1)*K+1 + totalOutputs*j]= -1
+        matrixHRepresentation[base + 1 + j, (K-1)*K+2 + totalOutputs*j] = -1
+        rowNum+=2                         
+                                                   
     vector1=np.array([0.5, 0, 0, 0, 0.5, 0, 0 ,0 ,0]) #NO DISYUNTOS
     vector2=np.array([0, 0.5, 0, 0.5, 0, 0, 0, 0, 0]) #SI DISYUNTOS
     Disjoint=np.concatenate((vector2,vector2,vector2,vector2,vector2,vector2,vector1,vector1,vector2,vector1,vector2,vector1,vector2,vector1,vector1,vector1))
     
     
-    P = pic.Problem()
-    A = pic.new_param('A', A)
-    Disjoint = pic.new_param('Disjoint', Disjoint)
-    C= pic.new_param('C', coefficients)
-    B = P.add_variable('B',NumberOfCoefficients)
-    P.add_constraint(A*B < C)
-    for j in range (0, NumberOfCoefficients/(K+1) + 1):
-        P.add_constraint(B[(K+1)*j-1]==0)
-    for j in range (0, N*N): #ESTOS CONSTRAINT DEPENDEN DE N, SI CAMBIO N HAY QUE CAMBIARLOS
-        P.add_constraint(B[(K+1)*K + (K+1)*(K+1)*j]==0)
-        P.add_constraint(B[(K+1)*(K+1) - 2 + (K+1)*(K+1)*j]==0)
-    objective = np.dot(Disjoint.T,B)
-    P.set_objective('max', objective)
-    P.solve(verbose=0,solver='cvxopt')
-    B_opt = B.value
-    print(B_opt)
+    mat = cdd.Matrix(matrixHRepresentation, number_type='fraction')
+    mat.obj_type = cdd.LPObjType.MAX
+    mat.obj_func = tuple(np.concatenate(([0],Disjoint)))
+    
+    lp = cdd.LinProg(mat)
+    lp.solve()
+    print(lp.status)
+    print(lp.obj_value)
+    print(" ".join("{0}".format(val) for val in lp.primal_solution))
+    
         
     
