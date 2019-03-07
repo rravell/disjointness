40bc05be6d5aecaecffed4e5dce485fa0a5ce65b
rravell <46346189+rravell@users.noreply.github.com>
2019-03-07 13:12:10 +0100

    Add files via upload

---
+++ src/maximiseinlisp.py
@@ -0,0 +1,26 @@
+'''
+Created on Mar 7, 2019
+
+@author: rravell
+'''
+import numpy as np
+
+
+if __name__ == '__main__':
+    N=4
+    K=2+1
+    vector1=np.array([0.5, 0, 0, 0, 0.5, 0, 0 ,0 ,0]) #NO DISYUNTOS
+    vector2=np.array([0, 0.5, 0, 0.5, 0, 0, 0, 0, 0]) #SI DISYUNTOS
+    Disjoint=np.concatenate((vector2,vector1,vector2,vector2,vector2,vector2,vector1,vector1,vector2,vector1,vector2,vector1,vector2,vector1,vector1,vector1))
+    text=open("maximiseinlisp.txt", 'w')
+    for j in range (0, (N*K)**2):
+        NumberOfVector=(j)//(K**2) 
+        NumberOfCoefficient=(j)%(K**2) 
+        NumberOfInputA=NumberOfVector//N 
+        NumberOfInputB=NumberOfVector%N                 
+        NumberOfOutputA=NumberOfCoefficient//K 
+        NumberOfOutputB=NumberOfCoefficient%K
+        Number=Disjoint[j]
+        syntax= "(A{}/{} B{}/{} = {} C)" .format(NumberOfOutputA, NumberOfInputA, NumberOfOutputB, NumberOfInputB, Number)
+        text.write(syntax +'\n' ) 
+    text.close()
