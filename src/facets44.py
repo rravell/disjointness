bbed57e6dfff6674cdaad3c12768578797cb6258
rravell <46346189+rravell@users.noreply.github.com>
2019-08-10 17:00:23 +0200

    Finding the facets

---
+++ src/facets44.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cvxopt as cvx
+import itertools as it
+from _functools import reduce
+from linopttools import *
+import qutip as qt
+import pycddlib as cdd
+
+
+if __name__ == '__main__':
+
+  outputsAlice = [4,4,4,4]
+  outputsBob = [4,4,4,4]
+  inputs=len(outputsAlice)
+  vertices=generateLocalVertices(outputsAlice,outputsBob)
+  symmetricVertices=Symmetrise(vertices,inputs,outputsAlice,outputsBob)
+  
+  print(symmetricVertices)
+  
+    
+  
