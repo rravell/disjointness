db62fefd32183cb62391eb1c009cf917b2caeba6
rravell <46346189+rravell@users.noreply.github.com>
2019-08-10 17:12:22 +0200

    Testing

--- src/facets44.py
+++ src/facets44.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
   vertices=generateLocalVertices(outputsAlice,outputsBob)
   symmetricVertices=Symmetrise(vertices,inputs,outputsAlice,outputsBob)
   
-  #print(symmetricVertices)
+  
   
     
   
